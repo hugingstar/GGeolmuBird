d63622a1b874c60e9777b088e0f32655e7f86a29
yslee <yslee4050@gmail.com>
2025-12-17 08:58:58 +0900

    tunning

--- Web/web_site.py
+++ Web/web_site.py
@@ -234,18 +234,18 @@ def calculate_indicators(data):
     # Setting values
     # RSI Divergence
     price_label_rsi = "MA5"
-    rsi_label = "RSI9"
+    rsi_label = "RSI3"
     rsi_rollback = 90
     price_label_rsi_hidden = "MA5"
-    rsi_label_hidden = "RSI9"
+    rsi_label_hidden = "RSI3"
     rsi_hidden_rollback = 180
 
     # CCI Divergence
     price_label_cci = "MA5"
-    cci_label = "CCI9"
+    cci_label = "CCI3"
     cci_rollback = 90
     price_label_cci_hidden = "MA5"
-    cci_label_hidden = "CCI9"
+    cci_label_hidden = "CCI3"
     cci_hidden_rollback = 180
 
     # Oversell Overbuy
