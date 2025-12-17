4408b0ae646b824e3d3fccc5250bfde6b4ca0d25
yslee <yslee4050@gmail.com>
2025-12-18 08:26:56 +0900

    refresh

--- Web/web_site.py
+++ Web/web_site.py
@@ -235,26 +235,26 @@ def calculate_indicators(data):
 
     # Setting values
     # RSI Divergence
-    price_label_rsi = "Close"
+    price_label_rsi = "MA5"
     rsi_label = "RSI"
     rsi_rollback = 90
-    price_label_rsi_hidden = "Close"
+    price_label_rsi_hidden = "MA5"
     rsi_label_hidden = "RSI"
     rsi_hidden_rollback = 180
 
     # CCI Divergence
-    price_label_cci = "Close"
+    price_label_cci = "MA5"
     cci_label = "CCI"
     cci_rollback = 90
-    price_label_cci_hidden = "Close"
+    price_label_cci_hidden = "MA5"
     cci_label_hidden = "CCI"
     cci_hidden_rollback = 180
 
     # Oversell Overbuy
-    rsi_overbuy_label = "RSI"
-    rsi_oversell_label = "RSI"
-    cci_overbuy_label = "CCI"
-    cci_oversell_label = "CCI"
+    rsi_overbuy_label = "RSI3"
+    rsi_oversell_label = "RSI3"
+    cci_overbuy_label = "CCI3"
+    cci_oversell_label = "CCI3"
 
     # 가격차이
     data['Close_diff_first'] = data['Close'].diff()
