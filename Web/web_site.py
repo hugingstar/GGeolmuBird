2b7bcd68f0045e38c557961d1e8c5ef67116a069
yslee <yslee4050@gmail.com>
2025-12-17 12:31:46 +0900

    ma5-rsi6-cci6

--- Web/web_site.py
+++ Web/web_site.py
@@ -234,18 +234,18 @@ def calculate_indicators(data):
     # Setting values
     # RSI Divergence
     price_label_rsi = "MA5"
-    rsi_label = "RSI4"
+    rsi_label = "RSI6"
     rsi_rollback = 90
     price_label_rsi_hidden = "MA5"
-    rsi_label_hidden = "RSI4"
+    rsi_label_hidden = "RSI6"
     rsi_hidden_rollback = 180
 
     # CCI Divergence
     price_label_cci = "MA5"
-    cci_label = "CCI4"
+    cci_label = "CCI6"
     cci_rollback = 90
     price_label_cci_hidden = "MA5"
-    cci_label_hidden = "CCI4"
+    cci_label_hidden = "CCI6"
     cci_hidden_rollback = 180
 
     # Oversell Overbuy
